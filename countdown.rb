9e7a257ad2dd83ddc58450f5bfefbde91e9ae2c0
github username <metelskyi88@gmail.com>
2019-01-03 14:38:17 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+#require 'pry'
+def countdown (number)
+  counter = 0
+  while counter < number do
+  puts "#{number - counter} SECOND(S)!"
+  counter += 1
+  #binding.pry
+end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+#countdown(10)
+
+def countdown_with_sleep(number)
+  counter = 0
+  while counter < number do
+  puts "#{number - counter} SECOND(S)!"
+  counter += 1
+  sleep number
+end
+  return "HAPPY NEW YEAR!"
 end
